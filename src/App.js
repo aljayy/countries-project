e70e7950ada74ce87e54874adb868dd9642939f1
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2022-12-22 18:20:30 -0800

    feat: add toggle theme functionality
    - This will add the functionality to change colors when the user
    decides to switch themes to the App.js, SearchCountry, and Header
    component.
    - The Header component already had this functionality but the inital
    commit didn't change the text color or icon.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import "./App.css";
+import { useContext } from "react";
+import ThemeContext from "./context/theme-context";
+import classes from "./App.module.scss";
 import Header from "./components/Header";
+import CountryFilters from "./components/CountryFilters/CountryFilters";
 
 function App() {
+  const { theme } = useContext(ThemeContext);
+
+  const themeClasses = theme === "light" ? classes.light : classes.dark;
+
+  console.log(themeClasses);
+
   return (
-    <div className="App">
+    <div className={themeClasses}>
       <Header />
+      <CountryFilters />
     </div>
   );
 }
--- src/components/CountryFilters/SearchCountry.jsx
+++ src/components/CountryFilters/SearchCountry.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../../context/theme-context";
 import lightmodesearch from "../../assets/lightmodesearch.svg";
+import darkmodesearch from "../../assets/darkmodesearch.svg";
 import classes from "./SearchCountry.module.scss";
 
 function SearchCountry() {
+  const { theme } = useContext(ThemeContext);
+  const themeClasses = theme === "light" ? classes.light : classes.dark;
+  const searchIcon = theme === "light" ? lightmodesearch : darkmodesearch;
+
   return (
-    <div className={classes["input-wrapper"]}>
+    <div className={`${classes["input-wrapper"]} ${themeClasses}`}>
       <div className={classes["search-icon"]}>
-        <img src={lightmodesearch} alt="Search Icon" />
+        <img src={searchIcon} alt="Search Icon" />
       </div>
       <input placeholder="Search for a country..." />
     </div>
