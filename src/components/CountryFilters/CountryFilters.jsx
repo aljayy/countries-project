842ee0a587c8e4c15d71c624bdd525c3c82a18a9
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2022-12-23 16:00:34 -0800

    feat: add the RegionFilter component

--- src/components/CountryFilters/CountryFilters.jsx
+++ src/components/CountryFilters/CountryFilters.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import RegionFilter from "./RegionFilter";
 import SearchCountry from "./SearchCountry";
 
 function CountryFilters() {
   return (
     <div>
       <SearchCountry />
+      <RegionFilter />
     </div>
   );
 }
