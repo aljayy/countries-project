3b271debbdd355f24bc8baf851163df287e29647
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2022-12-27 10:55:19 -0800

    feat: add functionality to retrieve country data by name

--- src/components/CountryFilters/SearchCountry.jsx
+++ src/components/CountryFilters/SearchCountry.jsx
@@ -1,11 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import ThemeContext from "../../context/theme-context";
 import lightmodesearch from "../../assets/lightmodesearch.svg";
 import darkmodesearch from "../../assets/darkmodesearch.svg";
 import classes from "./SearchCountry.module.scss";
+import CountryContext from "../../context/countries-context";
 
 function SearchCountry() {
   const { theme } = useContext(ThemeContext);
+  const { setCountryName } = useContext(CountryContext);
+  const inputRef = useRef();
   const themeClasses = theme === "light" ? classes.light : classes.dark;
   const searchIcon = theme === "light" ? lightmodesearch : darkmodesearch;
 
@@ -14,7 +17,14 @@ function SearchCountry() {
       <div className={classes["search-icon"]}>
         <img src={searchIcon} alt="Search Icon" />
       </div>
-      <input placeholder="Search for a country..." />
+      <input
+        placeholder="Search for a country..."
+        ref={inputRef}
+        onChange={() => {
+          console.log("COUNTRY NAME");
+          setCountryName(inputRef.current.value);
+        }}
+      />
     </div>
   );
 }
--- src/context/countries-context.js
+++ src/context/countries-context.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 const CountryContext = React.createContext({
   countries: [],
   loading: false,
+  setCountryName: () => {},
   filterByName: () => {},
   filterByRegion: () => {},
 });
 
 export function CountryContextProvider({ children }) {
   const [countries, setCountries] = useState([]);
+  const [countryName, setCountryName] = useState(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -36,6 +38,44 @@ export function CountryContextProvider({ children }) {
     setLoading(false);
   }
 
+  const filterByName = useCallback(async () => {
+    setLoading(true);
+    console.log("filterByName");
+    const response = await fetch(
+      `https://restcountries.com/v3.1/name/${countryName}`
+    );
+
+    console.log(response);
+
+    const data = await response.json();
+
+    const countries = data.map((country) => {
+      return {
+        flag: country.flags.svg,
+        name: country.name.official,
+        population: country.population.toLocaleString(),
+        region: country.region,
+        capital: country.capital,
+      };
+    });
+
+    setCountries(countries);
+    setLoading(false);
+  }, [countryName]);
+
+  useEffect(() => {
+    if (countryName !== null) {
+      const timer = setTimeout(() => {
+        console.log("use effect ran");
+        filterByName();
+      }, 500);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [countryName, filterByName]);
+
   async function filterByRegion(region) {
     setLoading(true);
     const response = await fetch(
@@ -59,7 +99,15 @@ export function CountryContextProvider({ children }) {
   }
 
   return (
-    <CountryContext.Provider value={{ countries, loading, filterByRegion }}>
+    <CountryContext.Provider
+      value={{
+        countries,
+        loading,
+        filterByRegion,
+        filterByName,
+        setCountryName,
+      }}
+    >
       {children}
     </CountryContext.Provider>
   );
