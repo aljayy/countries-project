139610dd5d3dabccaea79bce76fc2a4d56f34cf5
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2022-12-26 18:14:52 -0800

    feat: add context file for country data
    - This is done in order to make the retrieved country
    data from the API app wide state.
    - This commit also sees that the context provider is
    provided to the index.js file

---
+++ src/context/countries-context.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+const CountryContext = React.createContext({
+  countries: [],
+  loading: false,
+  filterByName: () => {},
+  filterByRegion: () => {},
+});
+
+export function CountryContextProvider({ children }) {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    retrieveAllCountries();
+  }, []);
+
+  async function retrieveAllCountries() {
+    setLoading(true);
+
+    const response = await fetch("https://restcountries.com/v3.1/all");
+
+    const data = await response.json();
+
+    const countries = data.map((country) => {
+      return {
+        flag: country.flags.svg,
+        name: country.name.official,
+        population: country.population.toLocaleString(),
+        region: country.region,
+        capital: country.capital,
+      };
+    });
+
+    setCountries(countries);
+    setLoading(false);
+  }
+
+  return (
+    <CountryContext.Provider value={{ countries, loading }}>
+      {children}
+    </CountryContext.Provider>
+  );
+}
+
+export default CountryContext;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { ThemeContextProvider } from "./context/theme-context";
+import { CountryContextProvider } from "./context/countries-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeContextProvider>
-        <App />
+        <CountryContextProvider>
+          <App />
+        </CountryContextProvider>
       </ThemeContextProvider>
     </BrowserRouter>
   </React.StrictMode>
