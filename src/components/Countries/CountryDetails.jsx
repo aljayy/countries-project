aac87818b65a0e216549f3666a50c4fcaf5be949
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2022-12-29 11:49:35 -0800

    refactor(api): move API fetching logic from CountryDetails.jsx to countries-context

--- src/components/Countries/CountryDetails.jsx
+++ src/components/Countries/CountryDetails.jsx
@@ -1,58 +1,17 @@
-import React, { useEffect, useCallback, useState } from "react";
+import React, { useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
+import CountryContext from "../../context/countries-context";
 import classes from "./CountryDetails.module.scss";
 import lightarrow from "../../assets/lightbackarrow.svg";
 
 function CountryDetails() {
-  const [countryDetails, setCountryDetails] = useState([]);
+  const { retrieveCountryDetails, loading, countryDetails } =
+    useContext(CountryContext);
   const { id } = useParams();
 
-  const retrieveCountryDetails = useCallback(async () => {
-    const response = await fetch(`https://restcountries.com/v3.1/alpha/${id}`);
-
-    const data = await response.json();
-
-    let countryData = data.map((country) => {
-      return {
-        borderCountries: country.borders,
-        capital: country.capital,
-        currency: Object.values(country.currencies)[0].name,
-        flag: country.flags.svg,
-        languages: Object.values(country.languages),
-        name: country.name.common,
-        native: Object.values(country.name.nativeName).pop().common,
-        population: country.population.toLocaleString(),
-        region: country.region,
-        subregion: country.subregion,
-        tld: country.tld,
-      };
-    });
-
-    let borderCountries = "";
-    for (let i = 0; i < countryData[0].borderCountries.length; i++) {
-      if (i === 0) {
-        borderCountries += countryData[0].borderCountries[i];
-      } else {
-        borderCountries += "," + countryData[0].borderCountries[i];
-      }
-    }
-
-    const borderResponse = await fetch(
-      `https://restcountries.com/v3.1/alpha?codes=${borderCountries}`
-    );
-
-    const borderData = await borderResponse.json();
-
-    countryData[0].borderCountries = borderData.map((country) => {
-      return { name: country.name.common, id: country.ccn3 };
-    });
-
-    setCountryDetails(countryData);
-  }, [id]);
-
   useEffect(() => {
-    retrieveCountryDetails();
-  }, [retrieveCountryDetails]);
+    retrieveCountryDetails(id);
+  }, [retrieveCountryDetails, id]);
 
   return (
     <>
@@ -102,21 +61,25 @@ function CountryDetails() {
               <span>Languages: </span>
               {countryDetails[0].languages}
             </p>
-            <h3>Border Countries:</h3>
-            <div className={classes.border}>
-              {countryDetails[0].borderCountries.map((border) => {
-                return (
-                  <Link
-                    to={`/country/${border.id}`}
-                    style={{ textDecoration: "none" }}
-                    preventScrollReset={false}
-                    className={classes["border-country"]}
-                  >
-                    {border.name}
-                  </Link>
-                );
-              })}
-            </div>
+            {countryDetails[0].borderCountries.length > 0 && (
+              <>
+                <h3>Border Countries:</h3>
+                <div className={classes.border}>
+                  {countryDetails[0].borderCountries.map((border) => {
+                    return (
+                      <Link
+                        to={`/country/${border.id}`}
+                        style={{ textDecoration: "none" }}
+                        preventScrollReset={false}
+                        className={classes["border-country"]}
+                      >
+                        {border.name}
+                      </Link>
+                    );
+                  })}
+                </div>
+              </>
+            )}
           </div>
         </div>
       )}
--- src/context/countries-context.js
+++ src/context/countries-context.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 
 const CountryContext = React.createContext({
   countries: [],
+  countryDetails: [],
   loading: false,
   setCountryName: () => {},
   filterByName: () => {},
@@ -11,6 +12,7 @@ const CountryContext = React.createContext({
 export function CountryContextProvider({ children }) {
   const [countries, setCountries] = useState([]);
   const [countryName, setCountryName] = useState(null);
+  const [countryDetails, setCountryDetails] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -84,14 +86,67 @@ export function CountryContextProvider({ children }) {
     setLoading(false);
   }
 
+  const retrieveCountryDetails = useCallback(async (id) => {
+    setLoading(true);
+    const response = await fetch(`https://restcountries.com/v3.1/alpha/${id}`);
+
+    const data = await response.json();
+
+    let countryData = data.map((country) => {
+      return {
+        borderCountries: country.borders ? country.borders : [],
+        capital: country.capital,
+        currency: Object.values(country.currencies)[0].name,
+        flag: country.flags.svg,
+        languages: Object.values(country.languages),
+        name: country.name.common,
+        native: Object.values(country.name.nativeName).pop().common,
+        population: country.population.toLocaleString(),
+        region: country.region,
+        subregion: country.subregion,
+        tld: country.tld,
+      };
+    });
+
+    if (countryData[0].borderCountries.length < 1) {
+      setCountryDetails(countryData);
+      setLoading(false);
+      return;
+    }
+
+    let borderCountries = "";
+    for (let i = 0; i < countryData[0].borderCountries.length; i++) {
+      if (i === 0) {
+        borderCountries += countryData[0].borderCountries[i];
+      } else {
+        borderCountries += "," + countryData[0].borderCountries[i];
+      }
+    }
+
+    const borderResponse = await fetch(
+      `https://restcountries.com/v3.1/alpha?codes=${borderCountries}`
+    );
+
+    const borderData = await borderResponse.json();
+
+    countryData[0].borderCountries = borderData.map((country) => {
+      return { name: country.name.common, id: country.ccn3 };
+    });
+
+    setCountryDetails(countryData);
+    setLoading(false);
+  }, []);
+
   return (
     <CountryContext.Provider
       value={{
         countries,
+        countryDetails,
         loading,
         filterByRegion,
         filterByName,
         setCountryName,
+        retrieveCountryDetails,
       }}
     >
       {children}
