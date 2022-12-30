cfc70c89924f9c74a1dcd1dbb747bd23ef822cf9
Alan Jimenez <75635276+aljayy@users.noreply.github.com>
2022-12-30 10:02:22 -0800

    fix: switch from BrowserRouter to HashRouter
    - This is done in order to get my routes working
    on gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeContextProvider } from "./context/theme-context";
 import { CountryContextProvider } from "./context/countries-context";
@@ -9,12 +8,12 @@ import { CountryContextProvider } from "./context/countries-context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeContextProvider>
         <CountryContextProvider>
           <App />
         </CountryContextProvider>
       </ThemeContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
